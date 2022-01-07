5cb610246f7d43fb141659c09532ecf6cc4352e1
hyde142857 <yoshihide.shirai+twitter@gmail.com>
2022-01-07 12:52:10 +0900

    add testcases

--- src/api/Utils.test.ts
+++ src/api/Utils.test.ts
@@ -1,6 +1,9 @@
 import { GITHUB_PAGES_URL } from "../types/Defs";
 import {
-  FightingIdIsInvalid, GetLpRank, TweetDataGetDefault, GetUrl
+  TweetDataGetDefault,
+  FightingIdIsInvalid,
+  GetTweetText, GetLpRank, GetUrl,
+  loadLocalStorage, saveLocalStorage, launchTwitterLive, submitTweet, launchNewIssue, launchGithub
 } from "./Utils"
 
 test('FightingIdIsInvalid()', () => {
@@ -28,4 +31,51 @@ test('GetUrl()', () => {
   expect(GetUrl(twdata)).toEqual(GITHUB_PAGES_URL);
   twdata.url = "hoge";
   expect(GetUrl(twdata)).toEqual("hoge");
+});
+
+test('GetTweetTextMessage()', () => {
+  let twdata = TweetDataGetDefault();
+  expect(GetTweetText(twdata)).toEqual(
+    "【ID】\n" +
+    "【連戦】1本先取\n" +
+    "【キャラセレ】OFF\n" +
+    "【ハード】どっちもOK\n" +
+    "【通信制限】OFF\n" +
+    "【パス】なし\n" +
+    "#ストVラウンジ募集\n"
+  );
+  twdata.lp = "hoge"
+  twdata.charactor = "リュウ"
+  twdata.gameround = "5ラウンド"
+  twdata.passcode = "9999"
+  twdata.comment = "hogehoge"
+  twdata.message = "hogehoge2"
+  expect(GetTweetText(twdata)).toEqual(
+    "hogehoge2\n\n" +
+    "【ID】\n" +
+    "【キャラ】リュウ\n" +
+    "【LP】hoge \n" +
+    "【ラウンド】5ラウンド 99秒\n" +
+    "【連戦】1本先取\n" +
+    "【キャラセレ】OFF\n" +
+    "【ハード】どっちもOK\n" +
+    "【通信制限】OFF\n" +
+    "【パス】9999\n" +
+    "【コメント】hogehoge\n" +
+    "#ストVラウンジ募集\n"
+  );
+});
+
+test('localStorage', () => {
+  localStorage.clear();
+  expect(loadLocalStorage()).toEqual(TweetDataGetDefault());
+  saveLocalStorage(TweetDataGetDefault());
+  expect(loadLocalStorage()).toEqual(TweetDataGetDefault());
+});
+
+test('utils function calls', () => {
+  launchTwitterLive();
+  submitTweet(TweetDataGetDefault());
+  launchNewIssue();
+  launchGithub();
 });
