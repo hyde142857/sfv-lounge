768987791614e2b5e700e329d7968eb13c9b96e7
hyde142857 <yoshihide.shirai+twitter@gmail.com>
2022-01-09 22:01:34 +0900

    Whitespace within parentheses is not allowed

--- src/api/Utils.ts
+++ src/api/Utils.ts
@@ -121,7 +121,7 @@ export function submitTweet(twdata:TweetData){
 }
 
 export function launchTwitterLive() {
-  window.open( "https://twitter.com/hashtag/%E3%82%B9%E3%83%88V%E3%83%A9%E3%82%A6%E3%83%B3%E3%82%B8%E5%8B%9F%E9%9B%86?src=hashtag_click&f=live", "_blank");
+  window.open("https://twitter.com/hashtag/%E3%82%B9%E3%83%88V%E3%83%A9%E3%82%A6%E3%83%B3%E3%82%B8%E5%8B%9F%E9%9B%86?src=hashtag_click&f=live", "_blank");
 }
 
 export function launchNewIssue() {
