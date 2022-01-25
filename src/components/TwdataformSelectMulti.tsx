c01249f7ab728cf275acb45369286d0a802760f3
hyde142857 <97071723+hyde142857@users.noreply.github.com>
2022-01-25 18:43:03 +0900

    Feature/switch multiselect component (#30)
    * refactor
    
    * completed!

--- src/components/TwdataformSelectMulti.tsx
+++ src/components/TwdataformSelectMulti.tsx
@@ -1,6 +1,8 @@
 import { Form } from "react-bootstrap";
-import Multiselect from 'multiselect-react-dropdown';
+import { Select, ModalCloseButton, MultiSelectOptionMarkup } from 'react-responsive-select';
 import { TweetData } from "../types/Defs";
+
+import 'react-responsive-select/dist/react-responsive-select.css';
 
 export type TwdataformSelectMultiProps = {
   label: string;
@@ -10,48 +12,50 @@ export type TwdataformSelectMultiProps = {
   updateTwdata: (key: keyof TweetData, val: string) => void;
 }
 
-function TwdataformSelectMulti(props: TwdataformSelectMultiProps) {
+function getOpts(props: TwdataformSelectMultiProps){
   let opts = [];
+  opts.push({ value: '', text: '選択なし' });
   for (const opt of props.options) {
-    opts.push({ name:opt, id:opt });
+    opts.push({ value: opt, text: opt, markup: <MultiSelectOptionMarkup text={opt} props={null} /> });
   }
+  return opts;
+}
 
+function getSelectedOpts(props: TwdataformSelectMultiProps){
   const value = props.twdata[props.twdataKey];
   const selected = value.replace("　"," ").split(' ');
   let selected_opts = [];
   if (value !== "") {
     for (const opt of selected) {
-      selected_opts.push({ name: opt.trim(), id: opt.trim() });
+      selected_opts.push(opt.trim());
     }
   }
+  return selected_opts;
+}
+
+function TwdataformSelectMulti(props: TwdataformSelectMultiProps) {
+  let opts = getOpts(props);
+  let selected_opts = getSelectedOpts(props);
 
   const onChange = (selectedList: any) => {
     const lselected = [];
-    for (const opt of selectedList) {
-      lselected.push(opt.id);
+    for (const opt of selectedList.options) {
+      lselected.push(opt.value);
     }
     var val = lselected.join(' ');
     props.updateTwdata(props.twdataKey,val);
   }
 
-  const onSelect = (selectedList: any, selectedItem: any) => {
-    onChange(selectedList);
-  };
-
-  const onRemove = (selectedList: any, removedItem: any) => {
-    onChange(selectedList);
-  };
-
   return (
     <Form.Group className="mb-3" controlId={props.label}>
       <Form.Label>{props.label}</Form.Label>
-      <Multiselect
-        options={opts}
+      <Select
+        multiselect={true}
+        name={props.twdataKey}
         selectedValues={selected_opts}
-        onSelect={onSelect}
-        onRemove={onRemove}
-        displayValue="name"
-        placeholder="選択してください"
+        modalCloseButton={<ModalCloseButton />}
+        options={opts}
+        onBlur={newValue => onChange(newValue)}
       />
     </Form.Group>
   );
