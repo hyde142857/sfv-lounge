11974ace16b46f351892c85ccee11c2340f73468
hyde142857 <yoshihide.shirai+twitter@gmail.com>
2022-01-29 21:39:03 +0900

    refactor

--- scripts/switchurl_dev.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i 's#__HOMEPAGE_FULL_URL__#https://hyde142857.github.io/sfv-lounge-dev#g' public/index.html
--- scripts/switchurl_master.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i 's#__HOMEPAGE_FULL_URL__#https://hyde142857.github.io/sfv-lounge#g' public/index.html
