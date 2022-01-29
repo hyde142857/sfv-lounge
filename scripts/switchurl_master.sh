e3e5333427b90fb01dcad2298ab320868e09f5dd
hyde142857 <yoshihide.shirai+twitter@gmail.com>
2022-01-29 21:31:44 +0900

    full url

--- scripts/switchurl_master.sh
+++ scripts/switchurl_master.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i 's#"homepage": .*#"homepage": "https://hyde142857.github.io/sfv-lounge",#g' package.json
+sed -i 's#__HOMEPAGE_FULL_URL__#https://hyde142857.github.io/sfv-lounge#g' public/index.html
