c8738b2b47776ff15d7f99302558f7e90555c005
hyde142857 <yoshihide.shirai+twitter@gmail.com>
2022-01-29 21:43:37 +0900

    recursible

--- scripts/switchurl.sh
+++ scripts/switchurl.sh
@@ -3,4 +3,4 @@
 GITHUB_USERID=`echo $GITHUB_REPOSITORY | sed 's#/.*##g'`
 GITHUB_REPOS=`echo $GITHUB_REPOSITORY | sed 's#.*/##g'`
 
-sed -i "s#__HOMEPAGE_FULL_URL__#https://$GITHUB_USERID.github.io/$GITHUB_REPOS#g" public/index.html
+find public -name "*.html" | xargs sed -i "s#__HOMEPAGE_FULL_URL__#https://$GITHUB_USERID.github.io/$GITHUB_REPOS#g"
