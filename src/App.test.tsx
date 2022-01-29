1164094e8d65ab6711c349ec0a8c6021d9675428
hyde142857 <97071723+hyde142857@users.noreply.github.com>
2022-01-29 13:11:18 +0900

    twitter card preview (#35)
    * twitter card preview
    
    * warning

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders application title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/スト5ラウンジ募集/i);
-  expect(linkElement).toBeInTheDocument();
 });
