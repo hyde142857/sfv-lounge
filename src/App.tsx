a3943a2e73e5b3aebb2abca9b05604871b70eb7b
hyde142857 <yoshihide.shirai+twitter@gmail.com>
2022-02-03 21:19:57 +0900

    2/3 21:19

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,63 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Navbar} from 'react-bootstrap';
 import TweetFormPage from './pages/TweetFormPage';
 import { CheckLiveTweetButton, RequestButton } from './components';
+import { AppBar, Container, createTheme, CssBaseline, IconButton, PaletteMode, ThemeProvider, Toolbar, Typography } from '@mui/material';
+import { createContext, useMemo, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-regular-svg-icons';
 
-function App() {
+const ColorModeContext = createContext({ toggleColorMode: () => {} });
 
+interface MainAppProps {
+  toggleColorMode: () => void,
+  palettemode: PaletteMode,
+}
+
+function ToggleColorMode() {
+  const [mode, setMode] = useState<'light' | 'dark'>('light');
+  const colorMode = useMemo(
+    () => ({
+      toggleColorMode: () => {
+        setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+      },
+    }),
+    [],
+  );
+
+  const theme = useMemo(
+    () =>
+      createTheme({
+        palette: {
+          mode,
+        },
+      }),
+    [mode],
+  );
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <MainApp palettemode={mode} toggleColorMode={colorMode.toggleColorMode}/>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+function MainApp(props:MainAppProps) {
   return (<>
-    <Navbar bg="dark" variant="dark" expand="lg" sticky='top'>
-      <Container>
-        <Navbar.Brand>スト5ラウンジ募集ツール</Navbar.Brand>
-      </Container>
-    </Navbar>
+    <CssBaseline />
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          スト5ラウンジ募集ツール
+        </Typography>
+        <div>
+        <IconButton sx={{ ml: 1 }} onClick={props.toggleColorMode} color="inherit">
+          {props.palettemode === 'dark' ? <FontAwesomeIcon icon={faMoon} /> : <FontAwesomeIcon icon={faSun} />}
+        </IconButton>
+        </div>
+      </Toolbar>
+    </AppBar>
     <br />
     <Container>
       <TweetFormPage />
@@ -27,6 +74,10 @@ function App() {
       Hyde (Twitter:<a href="https://twitter.com/hyde142857">Follow @hyde142857</a>)
     </div>
   </>);
+}
+
+function App() {
+  return (<ToggleColorMode />);
 }
 
 export default App;
--- src/pages/TweetFormPage.tsx
+++ src/pages/TweetFormPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Form, Row, Col } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import { TweetData } from '../types/Defs';
 import { FightingIdIsInvalid, loadLocalStorage } from '../api/Utils';
 import {
@@ -7,6 +7,7 @@ import {
   TwdataformText, TwdataformTextarea, TwdataformCheck,
   TwdataformSelect, TwdataformSelectMulti
 } from '../components/';
+import { Grid } from '@mui/material';
 
 function TweetFormPage() {
   const [twdata, setTwdata] = useState<TweetData>(loadLocalStorage());
@@ -17,132 +18,145 @@ function TweetFormPage() {
     setTwdata(ltwdata);
   }
 
-  return (<Form>
-    <TwdataformText twdata={twdata} updateTwdata={updateTwdata}
-      label="Fighter's ID" twdataKey='fightingId'
-      isInvalid={FightingIdIsInvalid(twdata)}
-      invalidFeedback="Fighter's IDの入力は、必須です。"
-    />
-    <TwdataformSelectMulti twdata={twdata} updateTwdata={updateTwdata}
-      label="キャラ" twdataKey='charactor'
-      options={[
-        "optHeader:オリジナル",
-        "リュウ", "春麗", "ナッシュ", "ベガ", "キャミィ", "バーディー", "ケン", "ネカリ",
-        "バルログ", "レインボー・ミカ", "ラシード", "かりん", "ザンギエフ", "ララ", "ダルシム", "ファン",
-        "optHeader:シーズン1",
-        "アレックス", "ガイル", "いぶき", "バイソン", "ジュリ", "ユリアン",
-        "optHeader:シーズン2",
-        "豪鬼", "コーリン", "エド", "アビゲイル", "メナト", "是空",
-        "optHeader:シーズン3",
-        "さくら", "ブランカ", "ファルケ", "コーディ", "G", "サガット",
-        "optHeader:シーズン4",
-        "影なる者", "ポイズン", "エドモンド本田", "ルシア", "ギル", "セス",
-        "optHeader:シーズン5",
-        "ダン", "ローズ", "オロ", "あきら", "ルーク", "イレブン"
-      ]}
-    />
-    <TwdataformText twdata={twdata} updateTwdata={updateTwdata}
-      label="LP" twdataKey='lp'
-      comment="(数字だけ入れるとランク名が補完されます。)"
-    />
-    <Row>
-      <Col>
+  return (
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <TwdataformText twdata={twdata} updateTwdata={updateTwdata}
+          label="Fighter's ID" twdataKey='fightingId'
+          isInvalid={FightingIdIsInvalid(twdata)}
+          invalidFeedback="Fighter's IDの入力は、必須です。"
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TwdataformSelectMulti twdata={twdata} updateTwdata={updateTwdata}
+          label="キャラ" twdataKey='charactor'
+          options={[
+            "optHeader:オリジナル",
+            "リュウ", "春麗", "ナッシュ", "ベガ", "キャミィ", "バーディー", "ケン", "ネカリ",
+            "バルログ", "レインボー・ミカ", "ラシード", "かりん", "ザンギエフ", "ララ", "ダルシム", "ファン",
+            "optHeader:シーズン1",
+            "アレックス", "ガイル", "いぶき", "バイソン", "ジュリ", "ユリアン",
+            "optHeader:シーズン2",
+            "豪鬼", "コーリン", "エド", "アビゲイル", "メナト", "是空",
+            "optHeader:シーズン3",
+            "さくら", "ブランカ", "ファルケ", "コーディ", "G", "サガット",
+            "optHeader:シーズン4",
+            "影なる者", "ポイズン", "エドモンド本田", "ルシア", "ギル", "セス",
+            "optHeader:シーズン5",
+            "ダン", "ローズ", "オロ", "あきら", "ルーク", "イレブン"
+          ]}
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TwdataformText twdata={twdata} updateTwdata={updateTwdata}
+          label="LP" twdataKey='lp'
+          comment="(数字だけ入れるとランク名が補完されます。)"
+        />
+      </Grid>
+      <Grid item xs={6}>
         <TwdataformSelect twdata={twdata} updateTwdata={updateTwdata}
           label="ラウンジ人数" twdataKey='loungePlayerMax'
           options={["", "2", "3", "4", "5", "6", "7", "8"]}
         />
-      </Col>
-      <Col>
+      </Grid>
+      <Grid item xs={6}>
         <TwdataformSelect twdata={twdata} updateTwdata={updateTwdata}
           label="プライベートスロット" twdataKey='loungePlayerPrivate'
           options={["", "1", "2", "3", "4", "5", "6", "7"]}
         />
-      </Col>
-    </Row>
-    <Row>
-      <Col>
+      </Grid>
+      <Grid item xs={6}>
         <TwdataformSelect twdata={twdata} updateTwdata={updateTwdata}
           label="ラウンド" twdataKey='gameround'
           options={["1ラウンド", "3ラウンド", "5ラウンド"]}
         />
-      </Col>
-      <Col>
+      </Grid>
+      <Grid item xs={6}>
         <TwdataformSelect twdata={twdata} updateTwdata={updateTwdata}
           label="タイム" twdataKey='gametime'
           options={["60秒", "99秒"]}
         />
-      </Col>
-    </Row>
-    <Row>
-      <Col>
+      </Grid>
+      <Grid item xs={6}>
         <TwdataformSelect twdata={twdata} updateTwdata={updateTwdata}
           label="連戦設定" twdataKey='game'
           options={["1本先取", "2本先取", "3本先取", "5本先取", "10本先取"]}
         />
-      </Col>
-      <Col>
+      </Grid>
+      <Grid item xs={6}>
         <TwdataformSelect twdata={twdata} updateTwdata={updateTwdata}
           label="セット" twdataKey='gameset'
           options={["", "x1", "x2", "x3", "x4", "x5", "xN"]}
         />
-      </Col>
-    </Row>
-    <Row>
-      <Col>
+      </Grid>
+      <Grid item xs={6}>
         <TwdataformSelect twdata={twdata} updateTwdata={updateTwdata}
           label="ハード" twdataKey='hardware'
           options={["どっちもOK", "PlayStationのみ", "PCのみ"]}
         />
-      </Col>
-      <Col>
+      </Grid>
+      <Grid item xs={6}>
         <TwdataformSelect twdata={twdata} updateTwdata={updateTwdata}
           label="通信制限" twdataKey='speedlimit'
           options={["ON", "OFF"]}
         />
-      </Col>
-    </Row>
-    <Row>
-      <Col>
+      </Grid>
+      <Grid item xs={6}>
         <TwdataformSelect twdata={twdata} updateTwdata={updateTwdata}
           label="キャラセレ" twdataKey='charactorSelect'
           options={["ON", "OFF"]}
         />
-      </Col>
-      <Col>
+      </Grid>
+      <Grid item xs={6}>
         <TwdataformText twdata={twdata} updateTwdata={updateTwdata}
           label="パス" twdataKey='passcode'
         />
-      </Col>
-    </Row>
-    <TwdataformText twdata={twdata} updateTwdata={updateTwdata}
-      label="URL" twdataKey='url'
-    />
-    <TwdataformCheck twdata={twdata} updateTwdata={updateTwdata}
-      label="ツイートに本ツールのURLを付与して、応援する。" twdataKey='attachToolUrl'
-      comment='(上記URLが空欄の時のみ有効です。)'
-    />
-    <TwdataformTextarea twdata={twdata} updateTwdata={updateTwdata}
-      label="先頭メッセージ" twdataKey='message'
-    />
-    <TwdataformTextarea twdata={twdata} updateTwdata={updateTwdata}
-      label="コメント" twdataKey='comment'
-    />
-    <Form.Group className="mb-3">
-      <Form.Label>プレビュー</Form.Label>
-      <TweetPreview twdata={twdata} />
-    </Form.Group>
-    <Form.Group className="mb-3">
-      <SaveButton twdata={twdata} />
-    </Form.Group>
-    <hr />
-    <Form.Group className="mb-3">
-      <TweetButton twdata={twdata} />
-    </Form.Group>
-    <hr />
-    <Form.Group className="mb-3">
-      <ClipboardButton twdata={twdata} />
-    </Form.Group>
-  </Form>
+      </Grid>
+      <Grid item xs={12}>
+        <TwdataformText twdata={twdata} updateTwdata={updateTwdata}
+          label="URL" twdataKey='url'
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TwdataformCheck twdata={twdata} updateTwdata={updateTwdata}
+          label="ツイートに本ツールのURLを付与して、応援する。" twdataKey='attachToolUrl'
+          comment='(上記URLが空欄の時のみ有効です。)'
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TwdataformTextarea twdata={twdata} updateTwdata={updateTwdata}
+          label="先頭メッセージ" twdataKey='message'
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <TwdataformTextarea twdata={twdata} updateTwdata={updateTwdata}
+          label="コメント" twdataKey='comment'
+        />
+      </Grid>
+      <Grid item xs={12}>
+        <Form.Group className="mb-3">
+          <Form.Label>プレビュー</Form.Label>
+          <TweetPreview twdata={twdata} />
+        </Form.Group>
+      </Grid>
+      <Grid item xs={12}>
+        <Form.Group className="mb-3">
+          <SaveButton twdata={twdata} />
+        </Form.Group>
+      </Grid>
+      <hr />
+      <Grid item xs={12}>
+        <Form.Group className="mb-3">
+          <TweetButton twdata={twdata} />
+        </Form.Group>
+      </Grid>
+      <hr />
+      <Grid item xs={12}>
+        <Form.Group className="mb-3">
+          <ClipboardButton twdata={twdata} />
+        </Form.Group>
+      </Grid>
+    </Grid>
   );
 }
 
